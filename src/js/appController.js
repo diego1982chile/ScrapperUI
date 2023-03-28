13f8afc848d06eefd9d010f7cbb2094e8e5aa970
diego1982chile <diego.friedman@gmail.com>
2023-03-28 01:01:15 -0300

    support for mobile

--- src/js/appController.js
+++ src/js/appController.js
@@ -108,11 +108,9 @@ define(['knockout', 'ojs/ojcontext', 'ojs/ojmodule-element-utils', 'ojs/ojknocko
     
     this.isAdmin = ko.observable(false);
 
-    this.scrapperServiceBaseUrl = ko.observable("https://cfg.internal.hhack.cl:8181/ScrapperConfig/api/");
+    this.scrapperServiceBaseUrl = ko.observable("PUT_YOUR_VALUE");
 
-    //this.scrapperServiceBaseUrl = ko.observable("http://instance-scrapper-apis.subnet01161944.vcn01161944.oraclevcn.com:8080/ScrapperConfig/api/");
-
-    this.tokenServiceBaseUrl = ko.observable("https://idp.internal.hhack.cl:8181/TokenService/api/");
+    this.tokenServiceBaseUrl = ko.observable("PUT_YOUR_VALUE");
       
     this.messages = ko.observableArray();
   
